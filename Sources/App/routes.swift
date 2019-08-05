62def7bb8931a939a12373a11ef909838aae94dd
Pawan Kumar <pawankumar@Drmacs-MacBook-Air.local>
2019-08-05 23:22:01 +0530

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,6 +12,11 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
 
+    router.get("welcome") { req in
+        return "Hello, world!"
+    }
+
+    
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
